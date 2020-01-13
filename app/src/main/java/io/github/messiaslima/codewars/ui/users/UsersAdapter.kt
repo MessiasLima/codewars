17745a234af4fe7772eaf2b2c6c261256bf5a59c
Messias Junior <messiaslima.03@gmail.com>
2020-01-13 18:32:50 +0000

    update user list item

--- app/src/main/java/io/github/messiaslima/codewars/ui/users/UsersAdapter.kt
+++ app/src/main/java/io/github/messiaslima/codewars/ui/users/UsersAdapter.kt
@@ -3,7 +3,9 @@ package io.github.messiaslima.codewars.ui.users
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import io.github.messiaslima.codewars.R
 import io.github.messiaslima.codewars.databinding.ListItemUserBinding
+import io.github.messiaslima.codewars.entity.Rank
 import io.github.messiaslima.codewars.entity.User
 
 class UsersAdapter(
@@ -35,7 +37,41 @@ class UsersAdapter(
     class UsersViewHolder(private val binding: ListItemUserBinding) : RecyclerView.ViewHolder(binding.root) {
 
         fun bindUser(user: User) {
+
+            val bestLanguage = getBestLanguage(user)
+
             binding.user = user
+            binding.listItemUserName.text = getValidName(user)
+            binding.listItemUserBestLanguage.text = binding.root.context.getString(
+                R.string.best_language,
+                bestLanguage.first,
+                bestLanguage.second
+            )
+        }
+
+        private fun getValidName(user: User): String? {
+            return if (user.name?.isNotBlank() == true) {
+                user.name
+            } else {
+                user.username
+            }
+        }
+
+        private fun getBestLanguage(user: User): Pair<String, Long> {
+
+            val languages = user.ranks?.languages
+
+            val sortedLanguages = languages?.toSortedMap(Comparator { o1, o2 ->
+                val language1 = languages[o1]
+                val language2 = languages[o2]
+                return@Comparator  language1?.score?.compareTo(language2?.score ?: 0) ?: 0
+            })
+
+
+            return Pair(
+                sortedLanguages!!.lastKey(),
+                sortedLanguages[sortedLanguages.lastKey()]!!.score!!
+            )
         }
 
         companion object {
